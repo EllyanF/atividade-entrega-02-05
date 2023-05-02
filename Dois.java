0f7cce189d48b734a05c803f35554b79b5d90ec2
EllyanF <ellyan.fernandes@hotmail.com>
2023-05-01 21:19:23 -0300

    Uses boolean variable to control while loop

--- Dois.java
+++ Dois.java
@@ -4,54 +4,55 @@ b ) A média do número de filhos.
 O final da leitura de dados dar-se-á com a entrada de salários negativos. 
  */
 
- import java.util.Scanner;
+import java.util.Scanner;
 
- public class Dois {
-     public static void main(String[] args) {
-         Scanner input = new Scanner(System.in);
-         float valorSalarios = 0;
-         int quantidadeFilhos = 0;
-         int quantidadeEntrevistados = 0;
-         
-         System.out.println("----- Pesquisa da prefeitura -----");
-         
-         loop: while (true) {
-             System.out.println("Escolha uma opção:\nresponder\nsair\nver resultado");
-             String resposta = input.nextLine();
- 
-             switch (resposta) {
-                 case "responder":
-                     System.out.println("Informe seu salário:");
-                     float salario = Float.parseFloat(input.nextLine());
-                     valorSalarios += salario;
- 
-                     if (salario < 0) {
-                         System.out.println("Fim. Não é possível recerber salário negativo!");
-                         break loop;
-                     }
- 
-                     System.out.println("Informe quantos filhos você tem:");
-                     quantidadeFilhos += input.nextInt();
-                     input.nextLine();
-                     quantidadeEntrevistados++;
-                     continue;
-                 case "sair":
-                     System.out.println("Fim do programa!");
-                     break loop;
-                 case "ver resultado":
-                     if (quantidadeEntrevistados == 0) {
-                         System.out.println("Ninguém foi entrevistado ainda");
-                         break;
-                     }
- 
-                     float mediaSalarial = valorSalarios / quantidadeEntrevistados;
-                     int mediaFilhos = quantidadeFilhos / quantidadeEntrevistados;
-                     System.out.printf("Houveram %d pessoa(s) entrevistadas.", quantidadeEntrevistados);
-                     System.out.printf("A média salarial é de R$%.2f por pessoa.", mediaSalarial);
-                     System.out.printf("A média de filhos por pessoa é de %d\n", mediaFilhos);
-             }
-         }
-         input.close();
-     }
- }
- 
+public class Dois {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        float valorSalarios = 0;
+        int quantidadeFilhos = 0;
+        int quantidadeEntrevistados = 0;
+        boolean runLoop = true;
+
+        System.out.println("----- Pesquisa da prefeitura -----");
+
+        while (runLoop) {
+            System.out.println("Escolha uma opção:\nresponder\nsair\nver resultado");
+            String resposta = input.nextLine();
+
+            switch (resposta) {
+                case "responder":
+                    System.out.println("Informe seu salário:");
+                    float salario = Float.parseFloat(input.nextLine());
+                    valorSalarios += salario;
+
+                    if (salario < 0) {
+                        System.out.println("Fim. Não é possível recerber salário negativo!");
+                        break;
+                    }
+
+                    System.out.println("Informe quantos filhos você tem:");
+                    quantidadeFilhos += input.nextInt();
+                    input.nextLine();
+                    quantidadeEntrevistados++;
+                    continue;
+                case "sair":
+                    System.out.println("Fim do programa!");
+                    runLoop = false;
+                    break;
+                case "ver resultado":
+                    if (quantidadeEntrevistados == 0 && runLoop == true) {
+                        System.out.println("Ninguém foi entrevistado ainda");
+                        break;
+                    }
+
+                    float mediaSalarial = valorSalarios / quantidadeEntrevistados;
+                    int mediaFilhos = quantidadeFilhos / quantidadeEntrevistados;
+                    System.out.printf("Houveram %d pessoa(s) entrevistadas.", quantidadeEntrevistados);
+                    System.out.printf("A média salarial é de R$%.2f por pessoa.", mediaSalarial);
+                    System.out.printf("A média de filhos por pessoa é de %d\n", mediaFilhos);
+            }
+        }
+        input.close();
+    }
+}
